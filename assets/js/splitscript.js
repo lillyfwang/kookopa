fac2a28b18a78beb830c8054d7d84bb976104a1b
lillyfwang <lillyfwang@yahoo.com>
2014-07-22 15:53:29 -0400

    updated website, more jquery, added photo gallery

--- assets/js/splitscript.js
+++ assets/js/splitscript.js
@@ -12,7 +12,85 @@
       }
     });
  
-    $( "#split-email" ).click(function() {
-      $( "#split-email-login" ).dialog( "open" );
-    });
+  $( "#split-email" ).click(function() {
+    $( "#split-email-login" ).dialog( "open" );
+  });
+
+  $( "#tabs" ).tabs();
+  
+  /** INFORMATION PAGE ANIMATION **/
+  $("#split-left").mouseenter(function() {
+    $("#split-left").animate({
+        width: '+=5%'
+    }, 200);
+    $("#split-right").animate({
+        width: '-=5%'
+    }, 200);
+  });
+  
+  $("#split-left").mouseleave(function() {
+    $("#split-left").animate({
+        width: '-=5%'
+    }, 200);
+    $("#split-right").animate({
+        width: '+=5%'
+    }, 200);
+  });
+  
+  $("#split-right").mouseenter(function() {
+    $("#split-right").animate({
+        width: '+=5%'
+    }, 200);
+    $("#split-left").animate({
+        width: '-=5%'
+    }, 200);
+  });
+  
+  $("#split-right").mouseleave(function() {
+    $("#split-right").animate({
+        width: '-=5%'
+    }, 200);
+    $("#split-left").animate({
+        width: '+=5%'
+    }, 200);
+  });
+  
+  /** RENTAL OPTIONS PAGE ANIMATION **/
+  $("#split-left2").mouseenter(function() {
+    $("#split-left2").animate({
+        width: '+=5%'
+    }, 200);
+    $("#split-right2").animate({
+        width: '-=5%'
+    }, 200);
+  });
+  
+  
+  $("#split-left2").mouseleave(function() {
+    $("#split-left2").animate({
+        width: '-=5%'
+    }, 200);
+    $("#split-right2").animate({
+        width: '+=5%'
+    }, 200);
+  });
+  
+  $("#split-right2").mouseenter(function() {
+    $("#split-right2").animate({
+        width: '+=5%'
+    }, 200);
+    $("#split-left2").animate({
+        width: '-=5%'
+    }, 200);
+  });
+  
+  $("#split-right2").mouseleave(function() {
+    $("#split-right2").animate({
+        width: '-=5%'
+    }, 200);
+    $("#split-left2").animate({
+        width: '+=5%'
+    }, 200);
+  });
+  
 }).call(this);
